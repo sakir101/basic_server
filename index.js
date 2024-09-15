fd9443e67ce4f12bdc3f7cc6d9c3806933284e53
sakir101 <sakirhossainf99@gmail.com>
2024-09-15 14:56:49 +0600

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello, World from server 1");
 });
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
